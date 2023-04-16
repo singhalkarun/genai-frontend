4fa04aafe5399ca4c2f4247c00a754f684d4c866
Abhishek Kumar <abhishek3243ak@gmail.com>
2023-04-16 18:30:48 +0530

    coming soon page

--- src/pages/components/logo-gen.tsx
+++ src/pages/components/logo-gen.tsx
@@ -1,9 +1,43 @@
 import React from 'react';
+import type { LottiePlayer } from 'lottie-web';
 
+declare global {
+    namespace JSX {
+      interface IntrinsicElements {
+        'lottie-player': React.DetailedHTMLProps<
+          React.HTMLAttributes<HTMLElement> & {
+            src: string;
+            background: string;
+            speed: string;
+            loop?: boolean;
+            autoplay?: boolean;
+          },
+          HTMLElement
+        >;
+      }
+    }
+  }
+
+  
 function LogoGenPage() {
+    
+    const loadingScreen = (
+        <div id="loading-screen">
+          <lottie-player
+            src="https://assets9.lottiefiles.com/packages/lf20_riAqnQrYxZ.json"
+            background="transparent"
+            speed="0.6"
+            style={{ width: "600px", height: "300px" }}
+            loop
+            autoplay
+          ></lottie-player>
+         
+        </div>
+      );
+    
     return (
         <div>
-            <h1>On the logo gen page</h1>
+            {loadingScreen}
         </div>
     );
 }
